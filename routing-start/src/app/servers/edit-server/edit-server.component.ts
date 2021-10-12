69017fc63834ce50a8e4369e905600c3a1f93183
Nicolas <wnarevalor@unal.edu.co>
2021-10-12 16:33:12 -0500

    added routing - routes are currently on app module

---
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { ServersService } from "../servers.service";
+
+@Component({
+  selector: "app-edit-server",
+  templateUrl: "./edit-server.component.html",
+  styleUrls: ["./edit-server.component.css"],
+})
+export class EditServerComponent implements OnInit {
+  server: { id: number; name: string; status: string };
+  serverName = "";
+  serverStatus = "";
+  allowEdit = false;
+
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.fragment.subscribe();
+    this.route.queryParams.subscribe((queryParams) => {
+      this.allowEdit = queryParams["allowEdit"] === "1" ? true : false;
+    });
+
+    this.server = this.serversService.getServer(1);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, {
+      name: this.serverName,
+      status: this.serverStatus,
+    });
+  }
+}
