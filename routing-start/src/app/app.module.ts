2c71aca6bba20cfa86d74e6f69e894514807cd29
Nicolas <wnarevalor@unal.edu.co>
2021-10-12 18:02:24 -0500

    outsourcing route configuration, adding guards (canActivate, canDeactivate), passing static data to routes and using resolvers

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -12,26 +12,13 @@ import { ServerComponent } from "./servers/server/server.component";
 import { ServersService } from "./servers/servers.service";
 import { RouterModule, Routes } from "@angular/router";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AuthService } from "./auth.service";
+import { AuthGuard } from "./auth-guard.service";
+import { CanDeactiveGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
+import { ServerResolver } from "./servers/server/server-resolver.service";
 
-const appRoutes: Routes = [
-  { path: "", component: HomeComponent },
-  {
-    path: "users",
-    component: UsersComponent,
-    children: [{ path: ":id/:name", component: UserComponent }],
-  },
-
-  {
-    path: "servers",
-    component: ServersComponent,
-    children: [
-      { path: ":id/edit", component: EditServerComponent },
-      { path: ":id", component: ServerComponent },
-    ],
-  },
-  { path: "not-found", component: PageNotFoundComponent },
-  { path: "**", redirectTo: "/not-found" },
-];
 @NgModule({
   declarations: [
     AppComponent,
@@ -42,9 +29,16 @@ const appRoutes: Routes = [
     EditServerComponent,
     ServerComponent,
     PageNotFoundComponent,
+    ErrorPageComponent,
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
-  providers: [ServersService],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [
+    ServersService,
+    AuthService,
+    AuthGuard,
+    CanDeactiveGuard,
+    ServerResolver,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, CanDeactivate, Router } from "@angular/router";
+import { Observable } from "rxjs/Observable";
 
 import { ServersService } from "../servers.service";
+import {
+  CanComponentDeactivate,
+  CanDeactiveGuard,
+} from "./can-deactivate-guard.service";
 
 @Component({
   selector: "app-edit-server",
   templateUrl: "./edit-server.component.html",
   styleUrls: ["./edit-server.component.css"],
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: { id: number; name: string; status: string };
   serverName = "";
   serverStatus = "";
   allowEdit = false;
+  changesSaved = false;
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -25,7 +32,9 @@ export class EditServerComponent implements OnInit {
       this.allowEdit = queryParams["allowEdit"] === "1" ? true : false;
     });
 
-    this.server = this.serversService.getServer(1);
+    this.server = this.serversService.getServer(
+      +this.route.snapshot.params["id"]
+    );
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
@@ -35,5 +44,22 @@ export class EditServerComponent implements OnInit {
       name: this.serverName,
       status: this.serverStatus,
     });
+    this.changesSaved = true;
+    this.router.navigate(["../"], { relativeTo: this.route });
+  }
+
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.allowEdit) {
+      return true;
+    }
+    if (
+      (this.serverName !== this.server.name ||
+        this.serverStatus !== this.server.status) &&
+      !this.changesSaved
+    ) {
+      return confirm("Do you want to discard the changes?");
+    } else {
+      return true;
+    }
   }
 }
